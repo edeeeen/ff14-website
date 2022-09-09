c0b152e82317b411e9b9a79971905ba44e790baf
edeeeen <okaythisisepic69420@gmail.com>
2022-09-09 17:25:43 -0400

    Initial Push
    Just some basic stuff

---
+++ main.js
@@ -0,0 +1,12 @@
+function searchForItem(){
+
+}
+
+function searchForRecipe(){
+    
+}
+
+function buttonClick() {
+    var word = document.getElementById("input").value;
+    document.getElementById("output").innerHTML = "You input " + word; 
+}
